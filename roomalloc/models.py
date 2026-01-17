fa1591f6d5058ba525e00db6cb03c45920d34264
AbhinavN2007 <n.abhinav2020@gmail.com>
2026-01-17 21:27:39 +0530

    Added Slot model, minor optimisation

--- roomalloc/models.py
+++ roomalloc/models.py
@@ -2,36 +2,41 @@ from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-	roomID = models.IntegerField(primary_key=True, unique=True)
-	roomName = models.CharField(max_length=64)
-	roomCapacity = models.IntegerField()
+	room_id = models.IntegerField(primary_key=True, unique=True)
+	room_name = models.CharField(max_length=64)
+	room_capacity = models.IntegerField()
 
 	def __str__(self):
-		return (f"roomid: {self.roomID}, Name: {self.roomName}, Capacity: {self.roomCapacity}")
+		return (f"roomid: {self.room_id}, Name: {self.room_name}, Capacity: {self.room_capacity}")
 
 class Student(models.Model):
-	rollno = models.CharField(max_length=10, primary_key=True, unique=True)
-	studentName = models.CharField(max_length=100)
-	studentDept = models.CharField(max_length=64)
+	rollno = models.CharField(max_length=10, primary_key=True)
+	student_name = models.CharField(max_length=100)
+	student_dept = models.CharField(max_length=64)
 
 	def __str__(self):
-		return (f"Roll No.: {self.rollno}, Name: {self.studentName}, Department: {self.studentDept}")
+		return (f"Roll No.: {self.rollno}, Name: {self.student_name}, Department: {self.student_dept}")
+
+
+class Slot(models.Model):
+	slot_id = models.AutoField(primary_key=True, unique=True)
+	start_time = models.DateTimeField()
+	end_time = models.DateTimeField()
+	#is_available needed?
+
+	def __str__(self):
+		return (f"Start Time:{self.start_time.strftime('%H:%M')}, End Time:{self.end_time.strftime('%H:%M')}")
+	
+
 
 class Booking(models.Model):
-	bookingID = models.AutoField(primary_key=True, unique=True)
-	bookingRoom = models.ForeignKey(Room, on_delete=models.RESTRICT) # references the roomID key (can be changed). remember while implementing later
-	bookingBy = models.ForeignKey(Student, on_delete=models.RESTRICT) # references the rollno key (can be changed). remember while implementing later
-	slot=models.ForeignKey(Slot, on_delete=models.RESTRICT)
+	booking_id = models.AutoField(primary_key=True, unique=True)
+	booking_room = models.ForeignKey(Room, on_delete=models.RESTRICT) # references the room_id key
+	booking_by = models.ForeignKey(Student, on_delete=models.RESTRICT) # references the rollno key
+	slot = models.ForeignKey(Slot, on_delete=models.RESTRICT)
 
 	def __str__(self):
-		return (f"Booking ID: {self.bookingID}, Room: {self.BookingRoom}, Student: {self.BookingBy}")
+		return (f"Booking ID: {self.booking_id}, Room: {self.booking_room}, Student: {self.booking_by}, Slot:{self.slot}")
 	
-class Slot(models.Model):
-	slotID=models.AutoField(primary_key=True, unique=True)
-	startTime=models.DateTimeField()
-	endTime=models.DateTimeField()
-	isAvailable=models.BooleanField(default=True)
 
-	def __str__(self):
-		return (f"Start Time:{self.startTime}, End Time:{self.endTime}, Availability:{self.isAvailable}")
 
