ff66a596ffa73ff7def577fde1ad4bfbfa4d9f21
Raghav Aggarwal <raghavaggarwal2k7@gmail.com>
2026-01-18 20:05:10 +0530

    tweaked models

--- roomalloc/models.py
+++ roomalloc/models.py
@@ -1,16 +1,18 @@
+import uuid
 from django.db import models
 
 # Create your models here.
 class Room(models.Model):
-	room_id = models.IntegerField(primary_key=True, unique=True)
+	room_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	room_name = models.CharField(max_length=64)
 	room_capacity = models.IntegerField()
+	is_booked = models.BooleanField()
 
 	def __str__(self):
 		return (f"roomid: {self.room_id}, Name: {self.room_name}, Capacity: {self.room_capacity}")
 
 class Student(models.Model):
-	rollno = models.CharField(max_length=10, primary_key=True)
+	rollno = models.CharField(max_length=10, primary_key=True, unique=True)
 	student_name = models.CharField(max_length=100)
 	student_dept = models.CharField(max_length=64)
 	email = models.EmailField(max_length=254, unique=True, null=True, blank=True)
@@ -20,7 +22,7 @@ class Student(models.Model):
 
 
 class Slot(models.Model):
-	slot_id = models.AutoField(primary_key=True, unique=True)
+	slot_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	start_time = models.DateTimeField()
 	end_time = models.DateTimeField()
 	DAY_CHOICES = [
@@ -33,7 +35,6 @@ class Slot(models.Model):
         ('SUN', 'Sunday'),
     ]
 	day = models.CharField(max_length=64,choices=DAY_CHOICES)
-	#is_available needed?
 
 	def __str__(self):
 		return (f"Start Time:{self.start_time.strftime('%H:%M')}, End Time:{self.end_time.strftime('%H:%M')}")
@@ -41,13 +42,10 @@ class Slot(models.Model):
 
 
 class Booking(models.Model):
-	booking_id = models.AutoField(primary_key=True, unique=True)
+	booking_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 	booking_room = models.ForeignKey(Room, on_delete=models.RESTRICT) # references the room_id key
 	booking_by = models.ForeignKey(Student, on_delete=models.RESTRICT) # references the rollno key
 	slot = models.ForeignKey(Slot, on_delete=models.RESTRICT)
 
 	def __str__(self):
 		return (f"Booking ID: {self.booking_id}, Room: {self.booking_room}, Student: {self.booking_by}, Slot:{self.slot}")
-	
-
-
